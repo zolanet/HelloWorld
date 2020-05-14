8664fc376fb25dc7f5962c8906d6cbdf960c7d83
Marc Doucet <marcauson@gmail.com>
2020-05-14 14:29:27 -0400

    Doubled font size made text green

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -24,8 +24,8 @@ void MainComponent::paint (Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
 
-    g.setFont (Font (16.0f));
-    g.setColour (Colours::red);
+    g.setFont (Font (32.0f));
+    g.setColour (Colours::green);
     g.drawText ("Hello World!", getLocalBounds(), Justification::centred, true);
 }
 
